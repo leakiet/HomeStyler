fc9e453d93afb0da063c124cd45a731f9c5a2891
quyendev <quyencutety@gmail.com>
2024-09-19 14:24:41 +0700

    update page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import HomePage from "./pages/HomePage/HomePage"
 import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
-// import DesignStyles from "./pages/DesignStyles/DesignStyles.jsx";
-// import HomePage from "./pages/HomePage/HomePage.jsx";
+import DesignStyles from "./pages/DesignStyles/DesignStyles.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
 import FreeQuote from "./components/FreeQuote/FreeQuote.js";
+import StyleDetail from "./components/StyleDetail/StyleDetail.js";
 
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       {/* <div className="container-fluid"> */}
       <div className="container-fluid">
         <Header />
-        <FreeQuote/>
+        {/* <FreeQuote/> */}
         {/* <HomePage /> */}
+        <StyleDetail/>
         {/* <DesignStyles/> */}
         <Footer />
       </div >
