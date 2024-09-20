62a1ded86ff1f1af5d0e82f0f4aaae9fb8a74ab9
cuoilen007 <71111575+cuoilen007@users.noreply.github.com>
2024-09-20 15:31:02 +0700

    aboutus

---
+++ src/components/About/AboutUs.jsx
@@ -0,0 +1,121 @@
+import './AboutUs.css';
+function AboutUs() {
+  return (
+    <div>
+
+      <section class="about_section layout_padding ">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="img-box">
+                <img src="images/about/about-img.jpg" alt="" />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="detail-box">
+                <div class="heading_container">
+                  <h2>
+                    About Us
+                  </h2>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem eum consequuntur ipsam repellat dolor soluta aliquid laborum, eius odit consectetur vel quasi in quidem, eveniet ab est corporis tempore.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="about_section layout_padding ">
+        <div class="container">
+          <div class="row">
+           
+            <div class="col-md-6">
+              <div class="detail-box">
+                <div class="heading_container">
+                  <h2>
+                    About Us
+                  </h2>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem eum consequuntur ipsam repellat dolor soluta aliquid laborum, eius odit consectetur vel quasi in quidem, eveniet ab est corporis tempore.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="img-box">
+                <img src="images/about/about-img.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+      <section class="service_section layout_padding">
+        <div class="container">
+          <div class="heading_container heading_center">
+            <h2>
+              We Are...
+            </h2>
+          </div>
+          <div class="row">
+            <div class="col-md-6 col-lg-4 mx-auto">
+              <div class="box">
+                <div class="img-box">
+                  <img src="images/about/g2.jpg" alt="" />
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    MR. A
+                  </h5>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio aspernatur officiis exercitationem asperiores praesentium
+                  </p>
+                
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mx-auto">
+              <div class="box">
+                <div class="img-box">
+                  <img src="images/about/g2.jpg" alt="" />
+                </div>
+                <div class="detail-box">
+                  <h5>
+                  MR. B
+                  </h5>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio aspernatur officiis exercitationem asperiores praesentium
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mx-auto">
+              <div class="box">
+                <div class="img-box">
+                  <img src="images/about/g3.jpg" alt="" />
+                </div>
+                <div class="detail-box">
+                  <h5>
+                  MR. C
+                  </h5>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio aspernatur officiis exercitationem asperiores praesentium
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </div>
+  );
+}
+
+export default AboutUs;
