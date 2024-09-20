1244c6e78a01da6005986b3c5fead591533c7d2f
cuoilen007 <71111575+cuoilen007@users.noreply.github.com>
2024-09-20 15:54:52 +0700

    Contact

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import DesignStyles from "./pages/DesignStyles/DesignStyles.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import FreeQuote from "./components/FreeQuote/FreeQuote.js";
 import StyleDetail from "./components/StyleDetail/StyleDetail.js";
-import { Routes } from "react-router-dom";
+import { Routes,Route } from "react-router-dom";
+import ContactUs from "./components/Contact/ContactUs.jsx";
 
 
 function App() {
@@ -16,7 +17,11 @@ function App() {
         <Header />
         {/* <FreeQuote/> */}
         {/* <HomePage /> */}
-        <StyleDetail/>
+        <Routes>
+          <Route path="/contact" element={<ContactUs/>}/>
+          <Route path="/" element={<StyleDetail/>}/>
+        
+        </Routes>
         {/* <DesignStyles/> */}
         <Footer />
       </div >
